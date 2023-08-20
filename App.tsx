7aa5adf831862dce65a2e835660e486eb9bb8b1d
Rishabh Singh <ris8645208@gmail.com>
2023-08-20 19:02:07 +0530

    feat: add AR & Web Mercator projection

--- App.tsx
+++ App.tsx
@@ -1,12 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
+import { Viro360Image, ViroARScene, ViroARSceneNavigator, ViroBox, ViroMaterials, ViroNode, ViroText, ViroTrackingStateConstants, ViroPolyline } from '@viro-community/react-viro';
+import React, { useEffect, useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -17,102 +10,87 @@ import {
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// Define a type for your points of interest
+type PointOfInterest = {
+  id: number;
+  latitude: number;
+  longitude: number;
+  label: string;
+  arCoordinates: { x: number; y: number; z: number }; // Include arCoordinates property
+};
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const ARScene = () => {
+  const [pointsOfInterest, setPointsOfInterest] = useState<PointOfInterest[]>([
+    { id: 1, latitude: 19.243139, longitude: 72.982861, label: 'Target 1', arCoordinates: { x: 0, y: 0, z: 0 } },
+    { id: 2, latitude: 19.243189, longitude: 72.982708, label: 'Target 2', arCoordinates: { x: 0, y: 0, z: 0 } },
+    { id: 3, latitude: 0, longitude: 0, label: 'Target 3', arCoordinates: { x: 0, y: 0, z: 0 } },
+    // Add more points of interest here
+  ]);
+
+  const initialCameraPosition = { x: 0, y: 0, z: 0 }; // Set your initial camera position
+
+  useEffect(() => {
+
+    // Convert GPS coordinates to AR world coordinates using Web Mercator projection
+    const convertGPStoARCoordinates = (latitude: PointOfInterest['latitude'], longitude: PointOfInterest['longitude']) => {
+      const metersPerLatDegree = 111200; // Approx. meters per degree latitude
+      const metersPerLngDegree = 111200 * Math.cos(latitude * (Math.PI / 180)); // Approx. meters per degree longitude
+
+      const x = (longitude - initialCameraPosition.x) * metersPerLngDegree;
+      const y = 0; // Elevation can be considered as y-coordinate
+      const z = (latitude - initialCameraPosition.z) * metersPerLatDegree;
+
+      return { x, y, z };
+    };
+
+
+    // Update AR world coordinates for each point of interest
+    const updatedPoints = pointsOfInterest.map((poi) => {
+      const { latitude, longitude } = poi;
+      const arCoordinates = convertGPStoARCoordinates(latitude, longitude);
+      return { ...poi, arCoordinates };
+    });
+
+    setPointsOfInterest(updatedPoints);
+  }, []);
+  console.log("Rendered")
+  console.log(pointsOfInterest)
+
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
+    <ViroARScene>
+      {pointsOfInterest.map((poi) => (
+        <ViroNode key={poi.id}>
+          <ViroText
+            text={poi.label}
+            scale={[1, 1, 1]}
+            position={
+              [
+                poi.arCoordinates.x,
+                poi.arCoordinates.y,
+                poi.arCoordinates.z,
+              ]}
+          />
+        </ViroNode>
+      ))
+      }
+    </ViroARScene >
   );
 }
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
+export default () => {
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: ARScene,
+      }}
+      style={styles.f1}
+    />
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+  f1: { flex: 1 },
 });
-
-export default App;
