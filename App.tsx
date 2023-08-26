10f28dbc97652b6a6ec9696583e404698f5bf7c5
Rishabh Singh <ris8645208@gmail.com>
2023-08-26 15:31:29 +0530

    feat: add gps based navigation

--- App.tsx
+++ App.tsx
@@ -1,14 +1,12 @@
-import { Viro360Image, ViroARScene, ViroARSceneNavigator, ViroBox, ViroMaterials, ViroNode, ViroText, ViroTrackingStateConstants, ViroPolyline } from '@viro-community/react-viro';
+import { Viro360Image, ViroARScene, ViroARSceneNavigator, ViroBox, ViroMaterials, ViroNode, ViroText, ViroTrackingStateConstants, ViroPolyline, ViroSphere } from '@viro-community/react-viro';
 import React, { useEffect, useState } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
-  View,
 } from 'react-native';
+import GetLocation, { Location } from 'react-native-get-location';
+import {
+  llToPX,
+} from 'web-merc-projection'
 
 // Define a type for your points of interest
 type PointOfInterest = {
@@ -21,25 +19,52 @@ type PointOfInterest = {
 
 
 const ARScene = () => {
+
   const [pointsOfInterest, setPointsOfInterest] = useState<PointOfInterest[]>([
     { id: 1, latitude: 19.243139, longitude: 72.982861, label: 'Target 1', arCoordinates: { x: 0, y: 0, z: 0 } },
     { id: 2, latitude: 19.243189, longitude: 72.982708, label: 'Target 2', arCoordinates: { x: 0, y: 0, z: 0 } },
-    { id: 3, latitude: 0, longitude: 0, label: 'Target 3', arCoordinates: { x: 0, y: 0, z: 0 } },
+    { id: 3, latitude: 19.122256, longitude: 72.9838502, label: 'Target 3', arCoordinates: { x: 0, y: 0, z: 0 } },
     // Add more points of interest here
   ]);
+  const [currentLocation, setCurrentLocation] = useState<Location>();
 
   const initialCameraPosition = { x: 0, y: 0, z: 0 }; // Set your initial camera position
 
   useEffect(() => {
 
+    GetLocation.getCurrentPosition({
+      enableHighAccuracy: true,
+      timeout: 60000,
+    })
+      .then(location => {
+        console.log("------------- location --------------");
+        console.log(location);
+        console.log("------------- location --------------");
+        setCurrentLocation(location);
+      })
+      .catch(error => {
+        const { code, message } = error;
+        console.warn(code, message);
+      })
+  }, []);
+
+  useEffect(() => {
+    if (!currentLocation)
+      return;
+
     // Convert GPS coordinates to AR world coordinates using Web Mercator projection
     const convertGPStoARCoordinates = (latitude: PointOfInterest['latitude'], longitude: PointOfInterest['longitude']) => {
-      const metersPerLatDegree = 111200; // Approx. meters per degree latitude
-      const metersPerLngDegree = 111200 * Math.cos(latitude * (Math.PI / 180)); // Approx. meters per degree longitude
 
-      const x = (longitude - initialCameraPosition.x) * metersPerLngDegree;
-      const y = 0; // Elevation can be considered as y-coordinate
-      const z = (latitude - initialCameraPosition.z) * metersPerLatDegree;
+      if (!currentLocation)
+        return { x: 0, y: 0, z: 0 };
+
+      const targetPos = llToPX([latitude, longitude], 9, false, 256);
+      const userPos = llToPX([currentLocation?.latitude, currentLocation?.longitude], 9, false, 256);
+      const x = targetPos[0] - userPos[0];
+      const z = targetPos[1] - userPos[1];
+      const y = 0;
+
+      console.log("{ x, y, z }", { x, y, z })
 
       return { x, y, z };
     };
@@ -53,9 +78,9 @@ const ARScene = () => {
     });
 
     setPointsOfInterest(updatedPoints);
-  }, []);
+  }, [currentLocation]);
   console.log("Rendered")
-  console.log(pointsOfInterest)
+  // console.log(pointsOfInterest)
 
   return (
     <ViroARScene>
@@ -64,12 +89,23 @@ const ARScene = () => {
           <ViroText
             text={poi.label}
             scale={[1, 1, 1]}
+            style={{ color: '#0000', textAlign: 'center', fontSize: 10 }}
+            position={
+              [
+                poi.arCoordinates.x,
+                poi.arCoordinates.y + 0.5,
+                poi.arCoordinates.z,
+              ]}
+          />
+          <ViroSphere
             position={
               [
                 poi.arCoordinates.x,
                 poi.arCoordinates.y,
                 poi.arCoordinates.z,
               ]}
+            radius={0.5}
+            scale={[0.4, 0.4, 0.4]}
           />
         </ViroNode>
       ))
