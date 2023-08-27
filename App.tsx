abc5acc449530c6192c5b0c7b5641692a1da2840
Rishabh Singh <ris8645208@gmail.com>
2023-08-27 18:26:57 +0530

    feat: add path to the POIs

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
-import { Viro360Image, ViroARScene, ViroARSceneNavigator, ViroBox, ViroMaterials, ViroNode, ViroText, ViroTrackingStateConstants, ViroPolyline, ViroSphere } from '@viro-community/react-viro';
+import { Viro360Image, ViroARScene, ViroARSceneNavigator, ViroBox, ViroMaterials, ViroNode, ViroText, ViroTrackingStateConstants, ViroPolyline, ViroSphere, ViroARPlane } from '@viro-community/react-viro';
+import { Viro3DPoint } from '@viro-community/react-viro/dist/components/Types/ViroUtils';
 import React, { useEffect, useState } from 'react';
 import {
   StyleSheet,
@@ -14,19 +15,21 @@ type PointOfInterest = {
   latitude: number;
   longitude: number;
   label: string;
-  arCoordinates: { x: number; y: number; z: number }; // Include arCoordinates property
+  arCoordinates: Viro3DPoint; // Include arCoordinates property
 };
 
 
 const ARScene = () => {
 
   const [pointsOfInterest, setPointsOfInterest] = useState<PointOfInterest[]>([
-    { id: 1, latitude: 19.243139, longitude: 72.982861, label: 'Target 1', arCoordinates: { x: 0, y: 0, z: 0 } },
-    { id: 2, latitude: 19.243189, longitude: 72.982708, label: 'Target 2', arCoordinates: { x: 0, y: 0, z: 0 } },
-    { id: 3, latitude: 19.122256, longitude: 72.9838502, label: 'Target 3', arCoordinates: { x: 0, y: 0, z: 0 } },
+    { id: 1, latitude: 19.243139, longitude: 72.982861, label: 'Target 1', arCoordinates: [0, 0, 0] },
+    { id: 2, latitude: 19.243189, longitude: 72.982708, label: 'Target 2', arCoordinates: [0, 0, 0] },
+    { id: 3, latitude: 19.122256, longitude: 72.9838502, label: 'Target 3', arCoordinates: [0, 0, 0] },
+    { id: 4, latitude: 19.243703, longitude: 72.982632, label: 'Target 4', arCoordinates: [0, 0, 0] },
     // Add more points of interest here
   ]);
   const [currentLocation, setCurrentLocation] = useState<Location>();
+  const [lineCoordinates, setLineCoordinates] = useState<Viro3DPoint[]>([]);
 
   const initialCameraPosition = { x: 0, y: 0, z: 0 }; // Set your initial camera position
 
@@ -56,7 +59,7 @@ const ARScene = () => {
     const convertGPStoARCoordinates = (latitude: PointOfInterest['latitude'], longitude: PointOfInterest['longitude']) => {
 
       if (!currentLocation)
-        return { x: 0, y: 0, z: 0 };
+        return [0, 0, 0] as Viro3DPoint;
 
       const targetPos = llToPX([latitude, longitude], 9, false, 256);
       const userPos = llToPX([currentLocation?.latitude, currentLocation?.longitude], 9, false, 256);
@@ -66,7 +69,7 @@ const ARScene = () => {
 
       console.log("{ x, y, z }", { x, y, z })
 
-      return { x, y, z };
+      return [x, y, z] as Viro3DPoint;
     };
 
 
@@ -78,38 +81,55 @@ const ARScene = () => {
     });
 
     setPointsOfInterest(updatedPoints);
+
+    const updatedLineCoordinates = updatedPoints.map((poi) => {
+      return poi.arCoordinates;
+    });
+
+    setLineCoordinates(updatedLineCoordinates);
+
   }, [currentLocation]);
-  console.log("Rendered")
-  // console.log(pointsOfInterest)
 
   return (
     <ViroARScene>
-      {pointsOfInterest.map((poi) => (
-        <ViroNode key={poi.id}>
-          <ViroText
-            text={poi.label}
-            scale={[1, 1, 1]}
-            style={{ color: '#0000', textAlign: 'center', fontSize: 10 }}
-            position={
-              [
-                poi.arCoordinates.x,
-                poi.arCoordinates.y + 0.5,
-                poi.arCoordinates.z,
-              ]}
-          />
-          <ViroSphere
-            position={
-              [
-                poi.arCoordinates.x,
-                poi.arCoordinates.y,
-                poi.arCoordinates.z,
-              ]}
-            radius={0.5}
-            scale={[0.4, 0.4, 0.4]}
-          />
-        </ViroNode>
-      ))
-      }
+      <ViroARPlane>
+        {
+          lineCoordinates[0] &&
+          (
+            <ViroPolyline
+              position={[0, 0, 0]}
+              points={lineCoordinates}
+              thickness={0.2}
+              materials={"red"}
+            />
+          )}
+        {pointsOfInterest.map((poi) => (
+          <ViroNode key={poi.id}>
+            <ViroText
+              text={poi.label}
+              scale={[1, 1, 1]}
+              style={{ color: '#0000', textAlign: 'center', fontSize: 10 }}
+              position={
+                [
+                  poi.arCoordinates[0],
+                  poi.arCoordinates[1] + 0.5,
+                  poi.arCoordinates[2]
+                ]}
+            />
+            <ViroSphere
+              position={
+                [
+                  poi.arCoordinates[0],
+                  poi.arCoordinates[1],
+                  poi.arCoordinates[2]
+                ]}
+              radius={0.2}
+              scale={[0.1, 0.1, 0.1]}
+            />
+          </ViroNode>
+        ))
+        }
+      </ViroARPlane>
     </ViroARScene >
   );
 }
@@ -129,4 +149,10 @@ export default () => {
 
 const styles = StyleSheet.create({
   f1: { flex: 1 },
+});
+
+ViroMaterials.createMaterials({
+  red: {
+    diffuseColor: 'red',
+  },
 });
